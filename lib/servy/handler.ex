73bdb08ca4dc17fc4ba7076b0717541940981945
Patrício dos Santos <hello@psantos.dev>
2024-08-25 12:32:27 +0100

    10. Module attributes

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,4 +1,13 @@
 defmodule Servy.Handler do
+  @moduledoc """
+  Handles HTTP requests.
+  """
+
+  @pages_path Path.expand("../../pages", __DIR__)
+
+  @doc """
+  Transforms the request into a response.
+  """
   def handle(request) do
     # conv = parse(request)
     # conv = route(conv)
@@ -23,6 +32,9 @@ defmodule Servy.Handler do
     end
   end
 
+  @doc """
+  Logs 404 erquests
+  """
   def track(%{status: 404, path: path} = conv) do
     IO.puts("Warning: #{path} is on the loose!")
     conv
@@ -57,7 +69,7 @@ defmodule Servy.Handler do
   end
 
   def route(%{method: "GET", path: "/bears/new"} = conv) do
-    Path.expand("../../pages", __DIR__)
+    @pages_path
     |> Path.join("form.html")
     |> File.read()
     |> handle_file(conv)
@@ -71,24 +83,8 @@ defmodule Servy.Handler do
     %{conv | status: 200, resp_body: "Deleted Bear #{id}"}
   end
 
-  # def route(%{method: "GET", path: "/about"} = conv) do
-  #   file =
-  #     Path.expand("../../pages", __DIR__)
-  #     |> Path.join("about.html")
-
-  #   case File.read(file) do
-  #     {:ok, content} ->
-  #       %{conv | status: 200, resp_body: content}
-
-  #     {:error, :enoent} ->
-  #       %{conv | status: 404, resp_body: "File not found!"}
-
-  #     {:error, reason} ->
-  #       %{conv | status: 500, resp_body: "Internal Server Error: #{:file.format_error(reason)}"}
-  #   end
-  # end
   def route(%{method: "GET", path: "/about"} = conv) do
-    Path.expand("../../pages", __DIR__)
+    @pages_path
     |> Path.join("about.html")
     |> File.read()
     |> handle_file(conv)
