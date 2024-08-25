dbdc191e537f7bdcbd6d270574705f5d41f469cb
Patrício dos Santos <hello@psantos.dev>
2024-08-25 19:21:44 +0100

    12. Modeling with structs

--- lib/servy/plugins.ex
+++ lib/servy/plugins.ex
@@ -1,21 +1,23 @@
 defmodule Servy.Plugins do
+  alias Servy.Conv
+
   @doc "Logs 404 requests"
-  def track(%{status: 404, path: path} = conv) do
+  def track(%Conv{status: 404, path: path} = conv) do
     IO.puts("Warning: #{path} is on the loose!")
     conv
   end
 
-  def track(conv), do: conv
+  def track(%Conv{} = conv), do: conv
 
-  def rewrite_path(%{path: "/wildlife"} = conv) do
+  def rewrite_path(%Conv{path: "/wildlife"} = conv) do
     %{conv | path: "/wildthings"}
   end
 
-  def rewrite_path(conv), do: conv
+  def rewrite_path(%Conv{} = conv), do: conv
 
-  def log(conv), do: IO.inspect(conv)
+  def log(%Conv{} = conv), do: IO.inspect(conv)
 
-  def rewrite_path_params(%{path: path} = conv) do
+  def rewrite_path_params(%Conv{path: path} = conv) do
     case String.split(path, "?id=") do
       [base_path, id] ->
         %{conv | path: "#{base_path}/#{id}"}
